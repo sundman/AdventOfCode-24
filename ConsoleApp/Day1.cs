69a9d7c3c82e9d8530de7351d23f8109509d61e0
Michael Sundman <sundman@sundman.at>
2024-12-01 17:25:21 +0100

    day1
    day1

---
+++ ConsoleApp/Day1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Day1
+    {
+        (List<int>,List<int>) handleInput()
+        {
+            var data = File.ReadAllLines("Input/Day1.txt");
+
+            decimal totalDiff = 0;
+            decimal part2Score = 0;
+
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            foreach (var line in data)
+            {
+                var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                list1.Add(int.Parse(parts[0]));
+                list2.Add(int.Parse(parts[1]));
+            }
+            return (list1, list2);
+        }
+
+         public decimal Part1()
+        {
+            var data = handleInput();
+            decimal totalDiff = 0;
+            data.Item1.Sort();
+            data.Item2.Sort();
+
+            for (int i = 0; i < data.Item1.Count; i++)
+            {
+                totalDiff += Math.Abs(data.Item1[i] - data.Item2[i]);
+
+            }
+
+            return totalDiff;
+        }
+
+       public decimal Part2()
+        {
+            decimal score = 0;
+            var data = handleInput();
+
+
+            for (int i = 0; i < data.Item1.Count; i++)
+            {
+
+                score += data.Item1[i] * data.Item2.Count(x => x == data.Item1[i]);
+            }
+
+            return score;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Diagnostics;
+using ConsoleApp;
+
+var day = new Day1();
+
+Stopwatch timer = new Stopwatch();  
+timer.Start();
+Console.WriteLine(day.Part1());
+Console.WriteLine(timer.ElapsedMilliseconds);
+Console.WriteLine(day.Part2());
+Console.WriteLine(timer.ElapsedMilliseconds);
+Console.ReadLine();
