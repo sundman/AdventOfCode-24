479a52b8998e796ed34742fe521ad6e0f48d3061
Michael Sundman <sundman@sundman.at>
2024-12-06 08:51:01 +0100

    day6

--- ConsoleApp/Day6.cs
+++ ConsoleApp/Day6.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Xml.XPath;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using String = System.String;
+﻿using System.Diagnostics;
 
 namespace ConsoleApp
 {
